3d9e85c53ee932dd6c7e4f66c56247a5fec8bf50
Nilam Chavan <nilam4444@gmail.com>
2021-02-08 18:53:53 +0530

    bg cup img imported

--- src/components/Section2/TitleWithCupbackground/TitleWithCupbackground.js
+++ src/components/Section2/TitleWithCupbackground/TitleWithCupbackground.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './TitleWithCupbackground.scss';
+import CupBg from './cup.svg'
 
 function TitleWithCupbackground() {
     return (
       <div className="title">
         <h1>Cups from <span className="mail-ru">mail.ru</span></h1>
-        <img src="img/cup.svg" alt="" />
+        <img src={CupBg} alt="Cup background" />
         <p className="subtitle">IT - specialists championships platform</p>
       </div>
     )
