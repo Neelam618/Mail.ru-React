a180afd87ddbf35ea3b43cc7886648fb911b2210
Nilam Chavan <nilam4444@gmail.com>
2021-02-08 17:10:46 +0530

    section1 and section2 folder added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/Header/Header';
-import Mainlogo from './components/Mainlogo/Mainlogo';
-import Mainimage from './components/Mainimage/Mainimage';
-import DiagonalLineBg from './components/DiagonalLineBg/DiagonalLineBg';
+import Header from './components/Section-1/Header/Header';
+import Mainlogo from './components/Section-1/Mainlogo/Mainlogo';
+import Mainimage from './components/Section-1/Mainimage/Mainimage';
+import DiagonalLineBg from './components/Section-2/DiagonalLineBg/DiagonalLineBg';
 
 
 function App() {
---
+++ src/components/Section-1/Section-1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Section1.scss';
+
+
+function Section1() {
+    return (
+      <div>
+        <Header />
+        <Mainlogo />
+        <Mainimage />
+      </div>
+    );
+  }
+  
+  export default Section1;
+  
