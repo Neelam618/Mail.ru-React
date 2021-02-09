e1864818ded455f7ecc4e0d77578de840ec3e283
Nilam Chavan <nilam4444@gmail.com>
2021-02-09 12:54:13 +0530

    Updates

---
+++ precache-manifest.2ad5982f4f07e0201dbfff34d84af60f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e98a2bd2a8f43640596244b6ce475ab",
+    "url": "/Mail.ru-React/index.html"
+  },
+  {
+    "revision": "a5738f760518cee7e779",
+    "url": "/Mail.ru-React/static/css/2.809f38ef.chunk.css"
+  },
+  {
+    "revision": "ae1dc231a17edc16df3c",
+    "url": "/Mail.ru-React/static/css/main.27ef02c8.chunk.css"
+  },
+  {
+    "revision": "a5738f760518cee7e779",
+    "url": "/Mail.ru-React/static/js/2.9c445fe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mail.ru-React/static/js/2.9c445fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1dc231a17edc16df3c",
+    "url": "/Mail.ru-React/static/js/main.d2e6ce98.chunk.js"
+  },
+  {
+    "revision": "9586d0177518f5a90507",
+    "url": "/Mail.ru-React/static/js/runtime-main.0335bfab.js"
+  },
+  {
+    "revision": "54b1d5a78417e4e72f62404aee7f7b53",
+    "url": "/Mail.ru-React/static/media/all-cups-big.54b1d5a7.png"
+  },
+  {
+    "revision": "185e379cc50bee012d53462ba031254e",
+    "url": "/Mail.ru-React/static/media/astronaut.185e379c.png"
+  },
+  {
+    "revision": "f1969374353c7a4279a01c3027dea293",
+    "url": "/Mail.ru-React/static/media/cup.f1969374.svg"
+  },
+  {
+    "revision": "0d366eb2bca6145842937a63fa06993b",
+    "url": "/Mail.ru-React/static/media/spaceship.0d366eb2.png"
+  }
+]);
